c764d40e103f6cfd2c1e718f60793561ba004881
Huy Phung <huyphung3103@gmail.com>
2023-09-08 00:41:22 -0400

    completed the operator overloading

--- Color.h
+++ Color.h
@@ -7,4 +7,9 @@ struct Color {
     Color(float pRed, float pGreen, float pBlue);
 
     void clamp();
+    Color operator+(Color c);
+    Color operator-(Color c);
+    Color operator*(const float scalar);
+
+    void printColor();
 };
